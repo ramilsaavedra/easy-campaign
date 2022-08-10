2f5c18b86fea85ce017ef69e55f9d433c318c085
Ramil Saavedra <ramilsmcl@gmail.com>
2022-08-10 09:56:40 +0800

    feat: google auth

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import express, { Application, Request, Response } from 'express';
+import './services/passport';
+import authRoute from './routes/authRoutes';
 
 const app: Application = express();
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World');
-});
-
-app.listen(5000, () => console.log('Server running'));
+authRoute(app);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
