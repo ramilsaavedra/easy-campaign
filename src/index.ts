f7d4d8a8bc7f43f5d1e70528dec8c14141792b43
Ramil Saavedra <ramilsmcl@gmail.com>
2022-08-11 13:25:58 +0800

    feat: mongo setup

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import express, { Application, Request, Response } from 'express';
+import mongoose from 'mongoose';
 import './services/passport';
 import authRoute from './routes/authRoutes';
+// @ts-ignore
+import keys from '../config/keys';
 
+mongoose.connect(keys.mongoURI);
 const app: Application = express();
 authRoute(app);
 const PORT = process.env.PORT || 5000;
