b8cb458a07eecb2b252793c320077207d5218bf7
Ramil Saavedra <ramilsmcl@gmail.com>
2022-08-15 22:31:50 +0800

    feat: tailwindcss setup

---
+++ client/layout/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <header>
+      <div className='container py-6 px-5'>
+        <h1 className='text-2xl text-gray-900'>Easy Campaign</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
